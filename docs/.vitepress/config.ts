53df767ea2b61e3c71dc2a8829f5e318fbc34d12
cyxbwww <18219326208@163.com>
2022-09-08 23:49:44 +0800

    docs: 添加书单

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
 		},
 		nav: [
 			{ text: '博客', link: '/blog/' },
+			{ text: '书单', link: '/book/' },
 			{ text: '个人项目', link: '/demo/' },
 			{ text: 'GitHub', link: 'https://github.com/cyxbwww/vitepress-blog' },
 			{ text: '掘金', link: 'https://juejin.cn/user/2058727243123982/posts' }
@@ -34,6 +35,29 @@ export default defineConfig({
 						{ text: 'Map和Set', link: '/blog/javascript/Map和Set' },
 						{ text: '函数', link: '/blog/javascript/函数' },
 						{ text: '期约与异步函数', link: '/blog/javascript/期约与异步函数' },
+					]
+				}
+			],
+			'/book/': [
+				{
+					'text': '你不知道的JavaScript',
+					collapsible: true,
+					items: [
+						{ text: '作用域和闭包', link: '/book/你不知道的JavaScript/作用域和闭包' },
+						{ text: 'this和对象原型', link: '/book/你不知道的JavaScript/this和对象原型' },
+						{ text: '类型和语法', link: '/book/你不知道的JavaScript/类型和语法' },
+						{ text: '异步和性能', link: '/book/你不知道的JavaScript/异步和性能' }
+					]
+				}, {
+					'text': 'Vue.js设计与实现',
+					collapsible: true,
+					items: [
+						{ text: '框架设计概览', link: '/book/Vue.js设计与实现/框架设计概览' },
+						{ text: '响应系统', link: '/book/Vue.js设计与实现/响应系统' },
+						{ text: '渲染器', link: '/book/Vue.js设计与实现/渲染器' },
+						{ text: '组件化', link: '/book/Vue.js设计与实现/组件化' },
+						{ text: '编译器', link: '/book/Vue.js设计与实现/编译器' },
+						{ text: '服务端渲染', link: '/book/Vue.js设计与实现/服务端渲染' },
 					]
 				}
 			],
---
+++ docs/book/index.md
@@ -0,0 +1,4 @@
+# 目录
+## [你不知道的JavaScript](/book/你不知道的JavaScript)
+
+## [Vue.js设计与实现](/book/Vue.js设计与实现)
