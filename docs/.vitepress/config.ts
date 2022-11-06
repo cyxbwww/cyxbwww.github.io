67456b3688e1e2cee7e42055d0319272828a7c7a
cyxbwww <18219326208@163.com>
2022-11-06 18:30:43 +0800

    docs: 目录调整

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 						{ text: '函数', link: '/blog/javascript/函数' },
 						{ text: '期约', link: '/blog/javascript/期约' },
 						{ text: '异步函数', link: '/blog/javascript/异步函数' },
-						{ text: '迭代器', link: '/blog/javascript/迭代器' },
+						{ text: '迭代器与生成器', link: '/blog/javascript/迭代器与生成器' },
 					]
 				},
 				{
